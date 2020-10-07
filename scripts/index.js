6cd750e8fca6c07ebb0140ee6f28ecd8df7e0d47
tolopedia <72498525+tolopedia@users.noreply.github.com>
2020-10-07 17:48:44 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'oct 07, 2020',
+  birthdate: 'oct 26, 2020',
   name: 'FELIA N'
 };
 
@@ -26,7 +26,7 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-let countDown = new Date(`${config.birthdate} 17:48:00`).getTime();
+let countDown = new Date(`${config.birthdate} 00:00:00`).getTime();
 x = setInterval(function() {
   let now = new Date().getTime(),
     distance = countDown - now;
